f520d8b0c77e18e2b0077ab23ddd0a731ee77aa1
mohan7603 <146535223+mohan7603@users.noreply.github.com>
2025-01-07 07:03:47 -0800

    Add files via upload

---
+++ PrimitiveData.java
@@ -0,0 +1,32 @@
+public class PrimitiveData{
+    public static void main(String[] args){
+        System.out.println("1.Byte :");
+        System.out.println("Size of byte "+Byte.SIZE+" "+"bits");
+
+        System.out.println("2.Short :");
+        System.out.println("Size of short "+Short.SIZE+" "+"bits");
+
+
+        System.out.println("3.Int :");
+        System.out.println("Size of int "+Integer.SIZE+" "+"bits");
+
+        System.out.println("4.Long :");
+        System.out.println("Size of long "+Long.SIZE+" "+"bits");
+
+        System.out.println("5.Double :");
+        System.out.println("Size of double "+Double.SIZE+" "+"bits");
+
+        System.out.println("6.Float :");
+        System.out.println("Size of float "+Float.SIZE+" "+"bits");
+
+        System.out.println("7.char :");
+        System.out.println("Size of char "+Character.SIZE+" "+"bits");
+
+        System.out.println("8.Boolean :");
+        System.out.println("Size of boolean: JVM depaends (not defined)");
+
+
+
+
+    }
+}
