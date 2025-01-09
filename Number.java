947e7fd4d134db7cdb76ef340387f95a281e6955
mohan7603 <146535223+mohan7603@users.noreply.github.com>
2025-01-09 05:33:27 -0800

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,14 @@
+// write a program to print 10 to 1 using Do While
+
+public class Number{
+    public static void main(String[] args){
+
+        int i=10;
+
+        do{
+            System.out.println(i);
+            i--;
+        }while(i>=1);
+
+    }
+} 
