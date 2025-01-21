ca2c4f43a680ac566b62dc381b126c526088a66a
Mohan <146535223+mohan7603@users.noreply.github.com>
2025-01-21 05:39:26 -0800

    Add files via upload

---
+++ Increase.java
@@ -0,0 +1,10 @@
+// Write a Program to Print 10,20,30....100 Using For Loop
+
+public class Increase{
+    public static void main(String[] args){
+
+        for(int i =10; i<=100; i=i+10){
+            System.out.println(i);
+        }
+    }
+}
